9dec56635891e20b16bac35774d84b160bda39b5
Arturo Romero <aromero@aeonlinesolutions.com>
2021-06-25 16:45:10 -0500

    first commit

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from EmployeeApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    url(r'^departamentos/$', views.departmentApi),
+    url(r'^departamentos/([0-9]+)$', views.departmentApi),
+
+    url(r'^empleados/$', views.employeeApi),
+    url(r'^empleados/( [0-9] + )$', views.employeeApi),
+
+    url(r'^SaveFile$', views.SaveFile)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
